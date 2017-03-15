4fdd7f0a65c4ce621f8733607b564fcdd678e62f
Sergey Georgiev <sgeorgiev@sevone.com>
2017-03-15 09:15:26 +0200

    added: copyright notice

--- Log/include/Log.h
+++ Log/include/Log.h
@@ -18,26 +18,12 @@
 
 #pragma once
 
-#include <string>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include "LogProcessor.h"
 
 namespace Log {
 
-class Pipe {
-private:
-	static const unsigned int BUFFER_SIZE = 256;
-public: 
-	Pipe();
-	~Pipe();
-
-public:
-	void write(const std::string& data);
-	std::string read();
-	
-private:
-	int fd_[2];
-};
-
+static inline void init(const std::string& configFileName) {
+	LogProcessor::instance().init(configFileName);
 }
+
+};
--- Log/include/LogProcessor.h
+++ Log/include/LogProcessor.h
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2017 Sergey Georgiev (sgeorgiev@cpp-examples.com)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ */
+
 #pragma once
 
 #include <Definitions.h>
@@ -49,10 +67,6 @@ private:
 	unsigned char timeBasedRotationMinute_;
 	unsigned char timeBasedRotationSecond_;
 };
-
-static inline void init(const std::string& configFileName) {
-	LogProcessor::instance().init(configFileName);
-}
 
 #define LOG(severity) (Log::LogProcessor::instance()) += Log::Message(severity)
 
